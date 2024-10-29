b10436b5eb06e6c383fe6fcb58bcb214155aee5d
Adarsh Raj <adarshraj@Adarshs-Air.lan>
2024-10-29 14:10:10 +0530

    Product Item Listing using GlueStack

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack, Tabs } from "expo-router";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
+
 
 
 
--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -1,8 +1,17 @@
-import {Text} from 'react-native'
+import { Card } from '@/components/ui/card';
+import { Image } from '@/components/ui/image';
+import { VStack } from '@/components/ui/vstack';
+import { Heading } from '@/components/ui/heading';
+import { Box } from '@/components/ui/box';
+import { Button, ButtonText } from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
+
 
 type Product = {
     name: string;
     price: number;
+    description: string;
+    image: string;
 };
 
 interface ProductListItemProps {
@@ -10,9 +19,56 @@ interface ProductListItemProps {
 }
 
 
-export default function ProductListItem({product}: ProductListItemProps){
-    return(
-        <Text style={{fontSize:30}}>{product.name} {product.price}</Text>
 
-    )
+
+export default function ProductListItem({product}: ProductListItemProps){
+    return (
+
+    <Card className="p-5 rounded-lg max-w-[360px] flex-1 md:flex-2">
+
+
+      <Image
+        source={{
+          uri: product.image
+        }}
+        alt={`${product.name} image`}
+        resizeMode='contain'
+        className="mb-6 h-[240px] w-full rounded-md"
+      />
+      <Text
+        className="text-sm font-normal mb-2 text-typography-700"
+      >
+        {product.name}
+
+      </Text>
+      <VStack className="mb-6">
+        <Heading size="md" className="mb-4">
+            ₹ {product.price}
+        </Heading>
+        <Text size="sm">
+          {product.description}
+        </Text>
+      </VStack>
+      <Box
+        className="flex-col sm:flex-row"
+      >
+        <Button
+          className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1"
+        >
+          <ButtonText size="sm">Add to cart</ButtonText>
+        </Button>
+        <Button
+          variant="outline"
+          className="px-4 py-2 border-outline-300 sm:flex-1"
+        >
+          <ButtonText
+            size="sm"
+            className="text-typography-600"
+          >
+            Wishlist
+          </ButtonText>
+        </Button>
+      </Box>
+    </Card>
+);
 }
